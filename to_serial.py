631bbb6dd774d0adf26bae0aeea803f3090def34
Thomas Warford <thomasdwarford@hotmail.co.uk>
2023-03-25 21:07:55 +0000

    Send data to arduino using python script.

---
+++ to_serial.py
@@ -0,0 +1,18 @@
+import serial
+import time
+
+PORT = '/dev/ttyUSB0'
+SLEEP_TIME = 0.05
+
+arduino = serial.Serial(port=PORT, baudrate=115200, timeout=.1)
+
+def write_read_to_arduino(data):
+	arduino.write(bytes(data, "utf-8"))
+	time.sleep(SLEEP_TIME)
+	return arduino.readline()
+n=0
+while True:
+
+	data_in = "<Hello world!>"
+	data_out = write_read_to_arduino(data_in)
+	print(data_out)
